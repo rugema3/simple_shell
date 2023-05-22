776ff0218af7bc234e481a05873368547f9a8c57
Jobs-rw <mjabush@yahoo.fr>
2023-05-22 03:53:35 -0700

    debug

---
+++ string_length.c
@@ -0,0 +1,27 @@
+#include"main.h"
+/**
+ * prefix_length - gets the length of a prefix substring.
+ * @str: initial segment.
+ * @acceptedChars: accepted bytes.
+ * Return: the number of accepted bytes.
+ */
+int prefix_length(char *str, char *acceptedChars)
+{
+	int strIndex, acceptIndex, flag;
+
+	for (strIndex = 0; *(str + strIndex) != '\0'; strIndex++)
+	{
+		flag = 1;
+		for (acceptIndex = 0; *(acceptedChars + acceptIndex) != '\0'; acceptIndex++)
+		{
+			if (*(str + strIndex) == *(acceptedChars + acceptIndex))
+			{
+				flag = 0;
+				break;
+			}
+		}
+		if (flag == 1)
+			break;
+	}
+	return (strIndex);
+}	
