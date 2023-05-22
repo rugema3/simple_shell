8e0a31e6761b443bf829effab34ddacc21ccf71e
Jobs-rw <mjabush@yahoo.fr>
2023-05-22 03:57:14 -0700

    commit on copy_string

---
+++ copy_string.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * copy_string - Copies the string pointed to by source.
+ * @destination: Type char pointer the destination of the copied str
+ * @source: Type char pointer the source of str
+ * Return: the destination.
+ */
+char *copy_string(char *destination, char *source)
+{
+	size_t charIndex;
+
+	for (charIndex = 0; source[charIndex] != '\0'; charIndex++)
+	{
+		destination[charIndex] = source[charIndex];
+	}
+	destination[charIndex] = '\0';
+
+	return (destination);
+} 
